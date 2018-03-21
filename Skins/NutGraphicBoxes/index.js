d9f1f59895f5c6aec4ec7077f71c1013f4b97c22
devboell <devboell@gmail.com>
2018-03-21 13:56:50 +0100

    added nut graphic for boxes skin

--- Skins/NutGraphicBoxes/index.js
+++ Skins/NutGraphicBoxes/index.js
@@ -1,7 +1,17 @@
 import React from 'react'
 
 
-const NutGraphicBoxes = () => (<g />)
+const NutGraphicBoxes = () =>
+  <rect
+    x="5%"
+    y="2%"
+    rx="3"
+    ry="3"
+    width="90%"
+    height="96%"
+    fill="lightgray"
+    stroke="none"
+  />
 
 
 export default NutGraphicBoxes
