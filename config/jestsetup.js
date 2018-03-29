20417589513d07784854816c50fb2f630d118ba4
devboell <devboell@gmail.com>
2018-03-29 19:34:23 +0200

    project setup changes for jest/react/enzyme upgrades

--- config/jestsetup.js
+++ config/jestsetup.js
@@ -1,4 +1,10 @@
-import { shallow, render, mount } from 'enzyme'
+import 'babel-polyfill'
+import 'raf/polyfill'
+
+import { shallow, render, mount, configure } from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16' // eslint-disable-line import/no-extraneous-dependencies
+
+configure({ adapter: new Adapter() })
 
 global.shallow = shallow
 global.render = render
