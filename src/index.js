65cb5c513563ade1fe844d3fc1a6cb9318b8adcf
devboell <devboell@gmail.com>
2018-03-31 15:51:48 +0200

    removed export of helpers function from previous version

--- src/index.js
+++ src/index.js
@@ -1,9 +1,2 @@
-export {
-  fretMatrixForPc,
-  fretMatrixForNote,
-  fretMatrixForInterval,
-  fretMatrixForChord,
-  fretMatrixForScale,
-} from 'lib/fretboard'
 
 export { default } from 'components/Fretboard'
