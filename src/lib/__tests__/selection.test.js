899a504ec3d3f93db8a64da633f6fa6d5fffd584
devboell <devboell@gmail.com>
2018-04-03 18:01:44 +0200

    added chordNotes

--- src/lib/__tests__/selection.test.js
+++ src/lib/__tests__/selection.test.js
@@ -1,15 +1,34 @@
-import { intervalNotes } from '../selection'
+import {
+  intervalNotes,
+  chordNotes,
+} from '../selection'
 
-it('should return array of interval pcs', () => {
-  const note = 'C'
-  const ivl = 4 // 3M
-  const expected = ['C', 'E']
-  expect(intervalNotes(note, ivl)).toEqual(expected)
+describe('selection, intervals', () => {
+  it('should return array of interval pcs', () => {
+    const note = 'C'
+    const ivl = 4 // 3M
+    const expected = ['C', 'E']
+    expect(intervalNotes(note, ivl)).toEqual(expected)
+  })
+
+  it('should return array of interval pitches', () => {
+    const note = 'C4'
+    const ivl = 4 // 3M
+    const expected = ['C4', 'E4']
+    expect(intervalNotes(note, ivl)).toEqual(expected)
+  })
 })
 
-it('should return array of interval pitches', () => {
-  const note = 'C4'
-  const ivl = 4 // 3M
-  const expected = ['C4', 'E4']
-  expect(intervalNotes(note, ivl)).toEqual(expected)
+describe('selection, chords', () => {
+  it('should return array of chord pcs', () => {
+    const chord = 'CM'
+    const expected = ['C', 'E', 'G']
+    expect(chordNotes(chord)).toEqual(expected)
+  })
+
+  it('should return array of chord pitches', () => {
+    const chord = 'C4M'
+    const expected = ['C4', 'E4', 'G4']
+    expect(chordNotes(chord)).toEqual(expected)
+  })
 })
--- src/lib/selection.js
+++ src/lib/selection.js
@@ -2,11 +2,13 @@
  * helper functions that return commonly used selection objects.
  */
 
-import { Distance, Interval } from 'tonal'
+import { Distance, Interval, Chord } from 'tonal'
 
-/* eslint-disable import/prefer-default-export */
 export const intervalNotes = (note, ivl) =>
   [
     note,
     Distance.transpose(note, Interval.fromSemitones(ivl)),
   ]
+
+export const chordNotes = chord =>
+  Chord.notes(chord)
