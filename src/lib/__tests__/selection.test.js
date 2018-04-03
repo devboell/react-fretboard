fdb698d5964257d6b408bedf99bd1502a016597e
devboell <devboell@gmail.com>
2018-04-03 17:44:23 +0200

    added intervalNotes test

--- src/lib/__tests__/selection.test.js
+++ src/lib/__tests__/selection.test.js
@@ -6,3 +6,10 @@ it('should return array of interval pcs', () => {
   const expected = ['C', 'E']
   expect(intervalNotes(note, ivl)).toEqual(expected)
 })
+
+it('should return array of interval pitches', () => {
+  const note = 'C4'
+  const ivl = 4 // 3M
+  const expected = ['C4', 'E4']
+  expect(intervalNotes(note, ivl)).toEqual(expected)
+})
