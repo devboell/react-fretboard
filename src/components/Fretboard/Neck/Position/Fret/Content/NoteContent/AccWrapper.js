9f70b423bb44026383eb8f86283309264ba7adb0
devboell <devboell@gmail.com>
2018-04-16 14:20:59 +0200

    increased acc fontSize by 2px

--- src/components/Fretboard/Neck/Position/Fret/Content/NoteContent/AccWrapper.js
+++ src/components/Fretboard/Neck/Position/Fret/Content/NoteContent/AccWrapper.js
@@ -3,7 +3,7 @@ import styled from 'styled-components'
 const AccWrapper = styled.span`
   text-align: center;
   vertical-align: top;
-  font-size: ${props => (props.theme.fontSize - 5)}px;
+  font-size: ${props => (props.theme.fontSize - 3)}px;
   overflow: hidden;
 `
 
